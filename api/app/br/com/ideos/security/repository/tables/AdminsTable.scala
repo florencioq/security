0534f6e331e9027497524668238db9251e5c204d
ErickLTrentini <erick.l.trentini@gmail.com>
2022-08-30 15:36:40 -0300

    fix: Fixes insertion on admins and managers tables

--- api/app/br/com/ideos/security/repository/tables/AdminsTable.scala
+++ api/app/br/com/ideos/security/repository/tables/AdminsTable.scala
@@ -5,6 +5,6 @@ import br.com.ideos.security.repository.SchemaName
 import slick.jdbc.PostgresProfile.api._
 
 class AdminsTable(tag: Tag) extends Table[AdminRef](tag, SchemaName, "admins") {
-  val userId = column[Long]("user_id", O.PrimaryKey, O.AutoInc)
+  val userId = column[Long]("user_id", O.PrimaryKey)
   def * = userId <> (AdminRef.apply, AdminRef.unapply)
 }
--- api/app/br/com/ideos/security/repository/tables/ManagersTable.scala
+++ api/app/br/com/ideos/security/repository/tables/ManagersTable.scala
@@ -5,7 +5,7 @@ import br.com.ideos.security.repository.SchemaName
 import slick.jdbc.PostgresProfile.api._
 
 class ManagersTable(tag: Tag) extends Table[Manager](tag, SchemaName, "managers") {
-  val userId = column[Long]("user_id", O.PrimaryKey, O.AutoInc)
+  val userId = column[Long]("user_id")
   val appKey = column[String]("app_key")
 
   def * = (userId, appKey) <> ((Manager.apply _).tupled, Manager.unapply)
